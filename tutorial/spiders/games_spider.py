f9de8d5ba96089d6114e750403e2f31c3b765da6
edkestebanpr23 <edkestebanpr23@gmail.com>
2020-03-17 09:59:46 -0500

    Scrapy gamesmen.com

--- tutorial/spiders/games_spider.py
+++ tutorial/spiders/games_spider.py
@@ -29,26 +29,42 @@ class QuotesSpider(scrapy.Spider):
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
+        
         for quote in response.css('li.item'): # Iterando sobre el array de todos los juegos de una pagina
             if quote.css('p.product-name a::text').get() is not None:
                 urlGame = quote.css('p.product-name a::attr(href)').get()
-                title = quote.css('p.product-name a::text').get()
+                # title = quote.css('p.product-name a::text').get()
 
                 yield scrapy.Request(str(urlGame), self.page)
 
-        # next_page = response.css('a.next::attr(href)')[0].get()
-        # print(next_page)
-        # if next_page is not None:
-        #     yield response.follow(next_page)
+        next_page = response.css('a.next::attr(href)')[0].get()
+        print(next_page)
+        if next_page is not None:
+            yield response.follow(next_page)
 
     def page(self, response):
         # Obtener genero
         datosGenero = response.css('tbody > tr > th::text').getall()
         indexGenre = datosGenero.index('Genre')
-        # genre = str(response.css('tbody > tr > td::text').getall()[indexGenre]).strip()
+        
+        genre = str(response.css('tbody > tr > td::text').getall()[indexGenre]).strip()
 
         # Obtener otros datos
-        # datos = response.css('div.attributelinkscontainer > div > div.gmdividerred h2::text').getall()
+        datos = response.css('div.attributelinkscontainer > div > div.gmdividerred h2::text').getall()
+        valores = response.css('div.attributelinkscontainer > div > div.attributelinks')
+
+
+        classification = None
+        publisher = None
+
+        if 'Classification' in datos:
+            indexCla = datos.index('Classification')
+            classification = valores[indexCla].css('span > img::attr(title)').get()
+
+        if 'Publisher' in datos:
+            indexPub = datos.index('Publisher')
+            publisher = valores[indexPub].css('a > img::attr(title)').get()
+
 
         # Desarrollador
         # indexDeveloper = datos.index('Developer')
@@ -56,7 +72,10 @@ class QuotesSpider(scrapy.Spider):
         yield {
             'title': response.css('div.product-name h1::text').get(),
             'url' : response.url,
-            'developer' : response.css('div.attributelinkscontainer > div > div.attributelinks a::attr(title)').get()
+            'developer' : response.css('div.attributelinkscontainer > div > div.attributelinks a::attr(title)').get(),
+            'genre' : genre,
+            'classification' : classification,
+            'publisher' : publisher
         }
 
 
